63965e92ecc4a2ae99ea8ca69154bb04851ac4b4
Adam Pietrasiak <info@impactweb.pl>
2013-05-24 21:34:33 +0200

    tour keyboard support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,42 @@
 //= require jquery
 (function($) {
 
+    $.fn.prevOrLast = function(selector)
+    {
+	var prev = this.prev(selector);
+	return (prev.length) ? prev : this.nextAll(selector).last();
+    };
 
+    $.fn.nextOrFirst = function(selector)
+    {
+	var next = this.next(selector);
+	return (next.length) ? next : this.prevAll(selector).last();
+    };
 
-$(document).ready(function(){
+    $(document).ready(function(){
     
-    $('.tour-menu td').click(function(){
-	var t = $(this);	
-	var offset = t.position().left + t.width()/2;
-	var arrow = $('.tour-arrow');
-	arrow.animate({'left': offset - arrow.width() / 2 + 'px'});
-    });
-    $('.tour-menu td').eq(0).trigger('click');
-})
+	$('.tour-menu td').click(function(){
+	    var t = $(this);
+	    t.addClass('clicked').siblings().removeClass('clicked');	
+	    var offset = t.position().left + t.width()/2;
+	    var arrow = $('.tour-arrow');
+	    arrow.animate({
+		'left': offset - arrow.width() / 2 + 'px'
+		});
+	});
+	$('.tour-menu td').eq(0).trigger('click');
+    
+	//keyboard support (left/right arrow nav)
+	$(document).keyup(function(e){			
+	    if ( $('.tour-arrow').is(':animated') ) { return; }
+	    if(e.keyCode == 37 || e.keyCode == 38) {//left
+		$('.tour-menu td.clicked').prevOrLast('td').trigger('click');
+	    }	
+	    if(e.keyCode == 39 || e.keyCode == 40) {//right
+		$('.tour-menu td.clicked').nextOrFirst('td').trigger('click');
+	    }	
+	});	    
+    })
 
 
 })(jQuery);

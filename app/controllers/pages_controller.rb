15a52a9b19d6e91578af74f4c162902d5d873780
Francesco Vittorio <frangucc@poggledinc.att.net>
2013-05-26 21:01:42 -0500

    massive amounts of image changes and works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+   
+   before_filter lambda { 
+      @body_class = params[:action] }
+
    def index
    	 @body_class = 'home_bg'
    end
@@ -16,15 +20,51 @@ class PagesController < ApplicationController
    end
    
    def works
-   @body_class = 'works_bg'
-   end
-   
-   def worksingle
-   @body_class = 'works_monthlys'
+      @body_class = 'works_bg'
    end
    
    def team
    
    end
+
+   def work_mantra
+   end
+   
+   def work_sellsimple
+   end
+
+   def work_bfi
+   end
+
+   def work_bsb
+   end
+
+   def work_ciw
+   end
+
+   def work_benchprep
+   end
+
+   def work_agentrun
+   end
+
+   def work_tagkast
+   end
+
+   def work_greenbeans
+   end
+
+   def work_chatyeo
+   end
+
+   def work_getable
+   end
+
+   def work_bevel
+   end
+
+
+
+
 
 end

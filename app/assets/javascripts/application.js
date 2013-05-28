188b706c01f1a42035b2a7f46324421066596598
Adam Pietrasiak <info@impactweb.pl>
2013-05-28 10:09:04 +0200

    misc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,16 +23,16 @@
 	return (next.length) ? next : this.prevAll(selector).last();
     };
     $.fadeBg = function(url,time,easing) {
-         var fader = $('#bgfader');
-	 var fader2 = $('#bgfader2');
+	var fader = $('#bgfader');
+	var fader2 = $('#bgfader2');
 	 
-	 if (!fader.is(':visible')) {
-	     fader.css('background-image','url(' + url + ')');
-	     fader.delay(800).fadeIn(time,easing);
-	 } else {
-	     fader2.css('background-image','url(' + url + ')');
-	     fader.delay(800).fadeOut(time,easing);
-	 }
+	if (!fader.is(':visible')) {
+	    fader.css('background-image','url(' + url + ')');
+	    fader.delay(800).fadeIn(time,easing);
+	} else {
+	    fader2.css('background-image','url(' + url + ')');
+	    fader.delay(800).fadeOut(time,easing);
+	}
     }
     $.fn.fadeSlideIn = function(time,easing,cb){
 	$(this).each(function(){
@@ -58,7 +58,9 @@
     $.fn.strethDown = function(marginDown)
     {	
 	var t = $(this);
-	if ( !t.length) { return $(); }
+	if ( !t.length) {
+	    return $();
+	}
 	var streth = function() {			
 	    var height=$(window).height() - t.offset().top - marginDown;		
 	    t.height(height);			
@@ -100,79 +102,50 @@
 	$(window).resize(function(){	    
 	    setRatio();
 	});
-	//$(this).resize(function(){setRatio()});
-	//$(this).parent().resize(function(){setRatio()});
+    //$(this).resize(function(){setRatio()});
+    //$(this).parent().resize(function(){setRatio()});
 	
 
 	
     }
     
     $.fn.vCenter = function() {
-         var t = $(this);
-	 var p = t.parent();
-	 var marg = (p.height() - t.height()) / 2;	 
-	 t.css('margin-top',marg);
+	var t = $(this);
+	var p = t.parent();
+	var marg = (p.height() - t.height()) / 2;	 
+	t.css('margin-top',marg);
 	$(window).resize(function(){	    
-	 var marg = (p.height() - t.height()) / 2;	 
-	 t.css('margin-top',marg);
+	    var marg = (p.height() - t.height()) / 2;	 
+	    t.css('margin-top',marg);
 	});	 
     }
     
     
     $(document).ready(function(){
  
+        $('.strethdown,.ux-slide-right').strethDown(0);
 
 	
-	var tour = function(i) {
-	    var url = '/assets/bgs/t' + i + '.jpg';
-	    $.fadeBg(url,1000,'easeInOutExpo');
-	    //$('body').css('background-image', 'url(/assets/bgs/t' + i + '.jpg)');
-		    
-	}
-	
-	$('.strethdown,.ux-slide-right').strethDown(0);
-	
-	$('.tour-menu td').click(function(){
-	    var t = $(this);
-	    var index = $('.tour-menu td').index(this) + 1;
-	    $('.tour-single-slogan').hide().eq(index).delay(100).show().fadeSlideIn();
-	    tour(index);
-	    t.addClass('clicked').siblings().removeClass('clicked');	
-	    var offset = t.position().left + t.width()/2;
-	    var arrow = $('.tour-arrow');
-	    arrow.animate({
-		'left': offset - arrow.width() / 2 + 'px'
-	    },600,'easeOutExpo');
-	    
-	});
-	
-	$('.tour-menu td').eq(0).trigger('click').promise().done(function(){
-	    $('.tour-arrow').fadeIn();
-	});
-    
-	//keyboard support (left/right arrow nav)
-	$(document).keyup(function(e){			
-	    if ( $('.tour-arrow:animated,#bgfader:animated,#bgfader2:animated').length ) {
-		return;
-	    }
-	    if(e.keyCode == 37 || e.keyCode == 38) {//left
-		$('.tour-menu td.clicked').prevOrLast('td').trigger('click');
-	    }	
-	    if(e.keyCode == 39 || e.keyCode == 40) {//right
-		$('.tour-menu td.clicked').nextOrFirst('td').trigger('click');
-	    }	
-	});
-	
 	$('#a-offerings').mouseenter(function(){
-	    $('.offerings').stop().animate({top:'52px'},400,'easeInOutExpo').addClass('opened');
+	    $('.offerings').stop().animate({
+		top:'52px'
+	    },400,'easeInOutExpo').addClass('opened');
 	});
 	$('.offerings,#a-offerings').mouseleave(function(){
-	    $(this).stop().animate({top:'-100%'},400,'easeInOutExpo').removeClass('opened');
+	    $(this).stop().animate({
+		top:'-100%'
+	    },400,'easeInOutExpo').removeClass('opened');
 	});
+	$('body').click(function(){
+	    $(this).stop().animate({
+		top:'-100%'
+	    },400,'easeInOutExpo').removeClass('opened');
+	})
 	
 	$('.embed-vid').fitVids();
- $.keepRatio({
-	    'ux-vid' : 16/10});	
+	$.keepRatio({
+	    'ux-vid' : 16/10
+	});	
 	
     });
     
